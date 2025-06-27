0a919e80273517b77451a50f27cdfb0facae97dc
BeesNutz-gif <1211109991@student.mmu.edu.my>
2025-06-28 04:22:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,28 +8,29 @@ import { CanvasUI } from './libs/CanvasUI.js';
 import { GazeController } from './libs/GazeController.js';
 import { XRControllerModelFactory } from './libs/three/jsm/XRControllerModelFactory.js';
 
-class App {
-    constructor() {
-        const container = document.createElement('div');
-        document.body.appendChild(container);
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
 
-        this.assetsPath = './assets/';
+		this.assetsPath = './assets/';
 
-        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 500);
-        this.camera.position.set(0, 1.6, 0);
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 500 );
+		this.camera.position.set( 0, 1.6, 0 );
 
         this.dolly = new THREE.Object3D();
         this.dolly.position.set(0, 0, 10);
-        this.dolly.add(this.camera);
+        this.dolly.add( this.camera );
         this.dummyCam = new THREE.Object3D();
-        this.camera.add(this.dummyCam);
+        this.camera.add( this.dummyCam );
 
-        this.scene = new THREE.Scene();
-        this.scene.background = null;
-        this.scene.add(this.dolly);
+		this.scene = new THREE.Scene();
+		this.scene.background = null; // Kill fallback background
 
-        const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
-        this.scene.add(ambient);
+        this.scene.add( this.dolly );
+
+		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
+		this.scene.add(ambient);
 
         const listener = new THREE.AudioListener();
         this.camera.add(listener);
@@ -38,41 +39,45 @@ class App {
         const audioLoader = new THREE.AudioLoader();
 
         audioLoader.load('music.mp3.mp3', (buffer) => {
-            sound.setBuffer(buffer);
-            sound.setLoop(true);
-            sound.setVolume(0.3);
+          sound.setBuffer(buffer);
+          sound.setLoop(true);
+          sound.setVolume(0.3);
         });
         document.body.addEventListener('click', () => {
-            if (!sound.isPlaying) sound.play();
+          if (!sound.isPlaying) sound.play();
         });
 
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-        container.appendChild(this.renderer.domElement);
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		container.appendChild( this.renderer.domElement );
         this.setEnvironment();
 
-        window.addEventListener('resize', this.resize.bind(this));
+        window.addEventListener('resize', () => this.resize());
 
         this.clock = new THREE.Clock();
-        this.up = new THREE.Vector3(0, 1, 0);
+        this.up = new THREE.Vector3(0,1,0);
         this.origin = new THREE.Vector3();
         this.workingVec3 = new THREE.Vector3();
         this.workingQuaternion = new THREE.Quaternion();
         this.raycaster = new THREE.Raycaster();
 
-        this.loadingBar = new LoadingBar();
-        this.loadCollege();
+		this.loadingBar = new LoadingBar();
+		this.loadCollege();
         this.immersive = false;
         const self = this;
         fetch('./college.json')
             .then(response => response.json())
-            .then(obj => {
+            .then(obj =>{
                 self.boardShown = '';
                 self.boardData = obj;
             });
-    }
+	}
+
+
+}
+
 
     setEnvironment() {
         this.scene.environment = null;
