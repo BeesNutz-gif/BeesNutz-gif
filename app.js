ab99af4396d79b0bea8dc3d92cee22b05f44d0d9
BeesNutz-gif <1211109991@student.mmu.edu.my>
2025-06-28 06:33:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -139,14 +139,17 @@ audioLoader.load('music.mp3.mp3', (buffer) => {
 						}else if (child.material.name.indexOf('Glass')!=-1){
                             child.material.opacity = 0.1;
                             child.material.transparent = true;
-                        }else if (child.material.name.indexOf("SkyBox")!=-1){
-                            const mat1 = child.material;
-                            const mat2 = new THREE.MeshBasicMaterial({map: mat1.map});
-                            child.material = mat2;
-                            mat1.dispose();
-                        }
-					}
-				});
+                       } else if (child.material.name.indexOf("SkyBox") != -1) {
+	const mat1 = child.material;
+	const mat2 = new THREE.MeshBasicMaterial({
+		map: mat1.map,
+		side: THREE.BackSide,
+		fog: false
+	});
+	child.material = mat2;
+	mat1.dispose();
+}
+
                        
                const door1 = college.getObjectByName("LobbyShop_Door__1_");
 const door2 = college.getObjectByName("LobbyShop_Door__2_");
