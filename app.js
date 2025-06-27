d43f6e15f3d3e03e7fdab74f67e2bde4a754000c
BeesNutz-gif <1211109991@student.mmu.edu.my>
2025-06-28 06:30:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ class App{
 
 	this.assetsPath = './assets/';
 	
-	this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 500);
+	this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 2000);
 	this.camera.position.set(0, 1.6, 0);
 	
 	this.dolly = new THREE.Object3D();
